fa6bea35231fe4c73816389ce0dd81c1e6507544
João Vitor Duarte Mariucio <joaovitorduartemariucio@gmail.com>
2019-10-11 21:47:37 -0300

    Fim do trabalho

--- conversorBases.php
+++ conversorBases.php
@@ -31,34 +31,77 @@
                         <input class="form-control" onKeyUp="validarNumeroInserido()" id="numero" name="numero" aria-describedby="emailHelp" placeholder="Digite aqui o numero">
                         <small id="emailHelp" class="form-text text-muted">Neste campo você deve informar um número referente a base que você selecionou.</small>
                     </div>
-                    <button class="btn float-right btn-primary" type="submit">Converter</button>
+                    <div class="collapse" id="numeroInvalido">
+                        <div class="alert alert-danger" style="text-align: center; role="alert">
+                            O número digitado é invalido para está operação
+                        </div>
+                    </div>
+                    <div class="collapse" id="buttonConverter">
+                        <button class="btn float-right btn-primary" type="submit">Converter</button>
+                    </div>
                 </form>
             </div>
         </div>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackp(ath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <script>
             function validarNumeroInserido() {
+                
                 var baseSelecionada = $('#baseNumero').find('option:selected').val();
                 var numeroDigitado = $('#numero').val();
-                
-                switch(parseInt(baseSelecionada)){
-                    case 2:
-                        console.log(/^[0-1]+$/g.test(numeroDigitado));
-                    break;
-                    case 8:
-                        console.log(/^[0-7]+$/g.test(numeroDigitado));
-                    break;
-                    case 10:
-                        console.log(/^[0-9]+$/g.test(numeroDigitado));
-                    break;
-                    case 16:
-                        console.log(/^[0-9a-fA-F]+$/g.test(numeroDigitado));
-                    break;
+        
+                if(numeroDigitado.trim().length){
+                    switch(parseInt(baseSelecionada)){
+                        case 2:
+                            var ehBinario = /^[0-1]+$/g.test(numeroDigitado);
+                            if(!ehBinario){
+                                $('#numeroInvalido').collapse('show');
+                            }else{
+                                $('#numeroInvalido').collapse('hide');
+                                $('#buttonConverter').collapse('show');
+                            }            
+                        break;
+                        case 8:
+                            var ehOctal = /^[0-7]+$/g.test(numeroDigitado);
+                            if(!ehOctal){
+                                $('#numeroInvalido').collapse('show');
+                            }else{
+                                $('#numeroInvalido').collapse('hide');
+                                $('#buttonConverter').collapse('show');
+                            }
+                        break;
+                        case 10:
+                            var ehDecimal = /^[0-9]+$/g.test(numeroDigitado);
+                            if(!ehDecimal){
+                                $('#numeroInvalido').collapse('show');
+                            }else{
+                                $('#numeroInvalido').collapse('hide');
+                                $('#buttonConverter').collapse('show');
+                            }
+                        break;
+                        case 16:
+                            var ehHexadecimal = /^[0-9a-fA-F]+$/g.test(numeroDigitado);
+                            if(!ehHexadecimal){
+                                $('#numeroInvalido').collapse('show');
+                            }else{
+                                $('#numeroInvalido').collapse('hide');
+                                $('#buttonConverter').collapse('show');
+                            }
+                        break;
+                    }
+                }else {
+                    $('#buttonConverter').collapse('hide');
                 }
             }
         </script>
+        <style>
+            th, td {
+                padding: 15px;
+                text-align: left;
+                border: 1px solid #ddd;
+            }
+        </style>
     </body>
 </html>
 
@@ -70,7 +113,6 @@
         conversorGeral($numero, $baseNumero);
     }
 
-    //--------HelperMethods---------start-------------------
     function suibstituirLetraPorNumero($letra){
         switch(strtoupper($letra)){
             case "A":
@@ -185,17 +227,34 @@ function converterQualquerNumeroParaBase10($number, $base){
 
     function conversorGeral($num, $base){
         $numNaBaseDez = 0;
-        $result = (object) array();
 
         if($base == 10)
             $numNaBaseDez = $num;
         else
         $numNaBaseDez = converterQualquerNumeroParaBase10(strval($num), $base);
 
-        $result->binario = converterNumeroParaBase2(strval($numNaBaseDez));;
-        $result->octal = converterNumeroParaBase8(strval($numNaBaseDez));
-        $result->decimal = $numNaBaseDez;
-        $result->hexadecimal = converterNumeroParaBase16(strval($numNaBaseDez));
+        $binario = converterNumeroParaBase2(strval($numNaBaseDez));;
+        $octal = converterNumeroParaBase8(strval($numNaBaseDez));
+        $decimal = $numNaBaseDez;
+        $hexadecimal = converterNumeroParaBase16(strval($numNaBaseDez));
         
-        var_dump($result);
+        echo "<div class=\"card\"  style=\"margin: 25px;\">
+            <h5 class=\"card-header\"><b>Resultado das conversões</b></h5>
+            <div class=\"card-body\">
+                <table border=\"1px\" style=\"height: 100% !important;width: 100% !important;text-align: center;\">
+                    <tr>
+                        <th>Número em Binário</th>
+                        <th>Número em Octal</th>
+                        <th>Número em Decimal</th>
+                        <th>Número em Hexadecimal</th>
+                    </tr>
+                    <tr>
+                        <td>$binario</td>
+                        <td>$octal</td>
+                        <td>$decimal</td>
+                        <td>$hexadecimal</td>
+                    </tr>
+                </table>
+            </div>
+        </div>";
     }
